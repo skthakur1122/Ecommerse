86b66dce93cababae65f50466c2c59d24e5d5375
souravkumar143 <souravkumarthakur185@gmail.com>
2021-06-08 16:40:26 +0530

    my first commit reactproject

---
+++ src/homepage.componenet.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const HomePage=()=>(
+
+<div className='homepage'>
+<div className='directory-menu'>
+    <div className='menu-iteam'>
+<div className='content'>
+    <h1 className='title'> HATS</h1>
+    <span className='sub-title'>SHOPNOW</span>
+</div>
+    </div>
+    <div className='menu-iteam'>
+<div className='content'>
+    <h1 className='title'> JACKETS</h1>
+    <span className='sub-title'>SHOPNOW</span>
+</div>
+    </div>
+    <div className='menu-iteam'>
+<div className='content'>
+    <h1 className='title'> SHOE</h1>
+    <span className='sub-title'>SHOPNOW</span>
+</div>
+    </div>
+    <div className='menu-iteam'>
+<div className='content'>
+    <h1 className='title'> MENS</h1>
+    <span className='sub-title'>SHOPNOW</span>
+</div>
+    </div>
+    <div className='menu-iteam'>
+<div className='content'>
+    <h1 className='title'> WOMENS</h1>
+    <span className='sub-title'>SHOPNOW</span>
+</div>
+    </div>
+
+</div>
+</div>
+
+
+
+
+);
+export default HomePage;
